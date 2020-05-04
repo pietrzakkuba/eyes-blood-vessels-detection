c8070c39782311a17de5067b006cc0595bebc0d5
pietrzakkuba <pietrzakkuba@yahoo.pl>
2020-05-04 15:52:58 +0200

    Picture.py create

--- main.py
+++ main.py
@@ -4,109 +4,18 @@ import cv2
 from os import listdir
 from os.path import isfile, join
 import sys
+from Picture import Picture
 
 
-class Picture:
-    def __init__(self, original_path, fovmask_path, target_path):
-        self.original_path = original_path
-        self.fovmask_path=fovmask_path
-        self.target_path = target_path
-
-    def processImage(self):
-        image = read_image(self.original_path, cv2.IMREAD_COLOR)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        height = image.shape[0]
-        width = image.shape[1]
-        image = resize(image, 800)
-        image = adjust_gamma(image, 1.07)
-        image = cv2.GaussianBlur(image, (3, 3), 0)
-        high_threshold, _ = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        high_threshold *= 0.75
-        low_threshold = high_threshold / 2
-        image = cv2.Canny(image, low_threshold, high_threshold)
-        contours, hierarchy = cv2.findContours(image, 1, 2)
-        cv2.drawContours(image, contours, -1, (255, 255, 0), 3)
-        kernel = np.ones((3, 3), np.uint8)
-        image = cv2.dilate(image, kernel, iterations=1)
-        image = cv2.erode(image, kernel, iterations=1)
-        ##############################################################
-        mask_path = self.fovmask_path  # TODO adresowanie tego ładnie jak inne
-        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-        mask = resize(mask, 800)
-        mask = cv2.erode(mask, kernel, iterations=3)
-        #############################################################
-        image = cv2.bitwise_and(image, mask)
-        image = resize(image, width, height)
-        return image
-
-
-
-def getPaths():
+def get_pictures():
     healthy = 'pictures\\healthy'
     healthy_fovmask = 'pictures\\healthy_fovmask'
     healthy_manual = 'pictures\\healthy_manualsegm'
-    pictures = []
-
     healthy_paths = [join(healthy, f) for f in listdir(healthy)]
     healthy_fovmask_paths = [join(healthy_fovmask, f) for f in listdir(healthy_fovmask)]
     healthy_manual_paths = [join(healthy_manual, f) for f in listdir(healthy_manual)]
-    for i in range(len(healthy_paths)):
-        pictures.append(Picture(healthy_paths[i], healthy_fovmask_paths[i], healthy_manual_paths[i]))
-    return pictures
-
-
-def resize(image, width, height=None, interpolation=cv2.INTER_AREA):
-    if height is None:
-        (h, w) = image.shape[:2]
-        ratio = w / width
-        height = int(np.round(h / ratio))
-    return cv2.resize(image, (width, height), interpolation=interpolation)
-
-
-def read_image(path, method):
-    image = cv2.imread(path, method)
-    return image
-
-
-def show_image(image):
-    image = resize(image, 800)
-    cv2.imshow('image', image)
-    cv2.waitKey(0)
-
-
-def adjust_gamma(image, gamma):
-    inv_gamma = 1.0 / gamma
-    table = np.array([((i / 255.0) ** inv_gamma) * 255
-                      for i in np.arange(0, 256)]).astype("uint8")
-    return cv2.LUT(image, table)
-
-
-def image_processing(path):
-    image = read_image(path, cv2.IMREAD_COLOR)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    height = image.shape[0]
-    width = image.shape[1]
-    image = resize(image, 800)
-    image = adjust_gamma(image, 1.07)
-    image = cv2.GaussianBlur(image, (3, 3), 0)
-    high_threshold, _ = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    high_threshold *= 0.75
-    low_threshold = high_threshold / 2
-    image = cv2.Canny(image, low_threshold, high_threshold)
-    contours, hierarchy = cv2.findContours(image, 1, 2)
-    cv2.drawContours(image, contours, -1, (255, 255, 0), 3)
-    kernel = np.ones((3, 3), np.uint8)
-    image = cv2.dilate(image, kernel, iterations=1)
-    image = cv2.erode(image, kernel, iterations=1)
-    ##############################################################
-    mask_path = '.\\pictures\\healthy_fovmask\\15_h_mask.tif'  # TODO adresowanie tego ładnie jak inne
-    mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-    mask = resize(mask, 800)
-    mask = cv2.erode(mask, kernel, iterations=3)
-    #############################################################
-    image = cv2.bitwise_and(image, mask)
-    image = resize(image, width, height)
-    return image
+    return [Picture(healthy_paths[i], healthy_fovmask_paths[i], healthy_manual_paths[i])
+            for i in range(len(healthy_paths))]
 
 
 def norm(image):
@@ -141,16 +50,10 @@ def evaluation(processed, actual):
     # TODO miary dla danych niezrównoważonych
 
 
-paths = getPaths()
-test_image = paths[0].processImage()
-show_image(test_image)
-actual_image = read_image(paths[0].target_path, cv2.IMREAD_GRAYSCALE)
-show_image(actual_image)
-evaluation(test_image, actual_image)
+pictures = get_pictures()
+pictures[0].process_image()
+pictures[0].show_image()
+pictures[0].show_target()
 
-# paths = get_paths('pictures')
-# test_image = image_processing(paths[-1][0])
-# # show_image(test_image)
-# actual_image = read_image(paths[-1][1], cv2.IMREAD_GRAYSCALE, 800)
-# # show_image(actual_image)
-# evaluation(test_image, actual_image)
+evaluation(pictures[0].image, pictures[0].target)
+
